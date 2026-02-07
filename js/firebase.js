4babd17167f55a765b27ddfd3b8683f07503fea3
Huang Zhaohong <huaug9672@gmail.com>
2026-02-08 04:01:15 +0800

    正式版

--- js/firebase.js
+++ js/firebase.js
@@ -1,16 +1,17 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyB9H2RRxs_7ERb2ZYztuCQu6zScMkOPW_Q",
-  authDomain: "campusmate-4.firebaseapp.com",
-  projectId: "campusmate-4",
-  storageBucket: "campusmate-4.firebasestorage.app",
-  messagingSenderId: "508067016216",
-  appId: "1:508067016216:web:eec789f59fadd8a13d9d6d",
-  measurementId: "G-LMQRW33ML0"
-};
+    apiKey: "AIzaSyBvWcCroeNSe4O1H_-hXgOJysO-Fyez0Qg",
+    authDomain: "campusking6.firebaseapp.com",
+    projectId: "campusking6",
+    storageBucket: "campusking6.firebasestorage.app",
+    messagingSenderId: "904334224237",
+    appId: "1:904334224237:web:21e9c3717bd05896af0864",
+    measurementId: "G-ER6B64XEBJ"
+  };
 
 firebase.initializeApp(firebaseConfig);// 初始化 Firebase 應用程式
 const auth = firebase.auth();// 取得 Auth 實例
 const provider = new firebase.auth.GoogleAuthProvider();// 建立 Google 登入提供者
 const db = firebase.firestore();// 取得 Firestore 資料庫實例
 
-const ADMIN_UID = "puqNkSNmpUaqK5Sw1srPH5cd6Jp2";
+const ADMIN_UID = "zxMKZHLkk1NvCKfAKEEWppXHCH73";
+
